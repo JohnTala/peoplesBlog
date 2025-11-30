79fa1b15379949cd9e70226e476f16bd4d13f683
JohnTala <57272492+JohnTala@users.noreply.github.com>
2025-11-30 23:58:37 +0200

    fix : vite.config for GitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   base: process.env.NODE_ENV === 'production' ? '/my-peoplesblog/' : '/',
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  }
+  server: { port: 3000 },
+  build: { outDir: '../dist', emptyOutDir: true },
 });
