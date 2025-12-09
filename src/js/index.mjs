2b756cc907a37a4c0a87258ed15dcde859d40987
byu-st <stalatala@byupathway.edu>
2025-12-09 21:58:17 +0200

    complete  merge-conflicts on index.mjs

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,28 +1,4 @@
-// // src/js/index.mjs
 
-// import { getPosts } from "./utils.js";
-
-// const myPreview = document.querySelector(".preview");
-// const my_url = "https://jsonplaceholder.typicode.com/posts/";
-// const today=new Date();
-// document.addEventListener("DOMContentLoaded", async () => {
-//   const posts = await getPosts(my_url);
-
-//   console.log(posts);
-
-//   myPreview.innerHTML = posts
-//     .map(post => `
-//         <div class="preview_post" key=${post.id}>
-//         <h2>${post.title}</h2>
-//         <p>${post.body.slice(0,200)}...</p>
-//        <a href="singlepost.html?id=${post.id}">Read More...</a>
-//         <p><em>${today.toDateString()}</em></p>
-//         </div>
-//         `)
-//     .join("");
-// });
-
-// src/js/index.mjs
 import { getPosts } from "./utils.js";
 
 const myPreview = document.querySelector(".preview");
@@ -30,6 +6,7 @@ const my_url = "https://jsonplaceholder.typicode.com/posts/";
 const today = new Date();
 
 document.addEventListener("DOMContentLoaded", async () => {
+  
   const posts = await getPosts(my_url);
 
   console.log(posts);
@@ -46,4 +23,4 @@ document.addEventListener("DOMContentLoaded", async () => {
       `
     )
     .join("");
-}
+});
